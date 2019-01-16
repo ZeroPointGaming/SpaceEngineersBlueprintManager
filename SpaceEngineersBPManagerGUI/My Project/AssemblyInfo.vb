36378d76b008f4bcceb01c79687d87ead228fe9b
ZeroPointGaming <comfyseven4@gmail.com>
2019-01-16 16:41:41 -0500

    Add files via upload

---
+++ SpaceEngineersBPManagerGUI/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SpaceEngineersBPManagerGUI")>
+<Assembly: AssemblyDescription("Beta Version 1.3")>
+<Assembly: AssemblyCompany("HedrickDevelopmentSolutions")>
+<Assembly: AssemblyProduct("SpaceEngineersBPManagerGUI")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("HDS")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cffb6bd1-47e8-4cbb-9bea-3361e1a4d192")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.3.0.0")>
+<Assembly: AssemblyFileVersion("1.3.0.0")>
