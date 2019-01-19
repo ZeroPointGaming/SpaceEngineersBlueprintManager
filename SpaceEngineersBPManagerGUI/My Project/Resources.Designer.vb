bfe0d454afb30ad57913e17b053d197ffd860eb5
LordComfy <comfyseven4@gmail.com>
2019-01-19 15:26:15 -0500

    Added missing resources > ArmorCenter > ArmorCorner > ArmorInvCorner > ArmorSide > LargeBlockRadioAntenna > LargeBlockSmallAtmosphericThrust > LargeSteelCatwalk

--- SpaceEngineersBPManagerGUI/My Project/Resources.Designer.vb
+++ SpaceEngineersBPManagerGUI/My Project/Resources.Designer.vb
@@ -22,7 +22,7 @@ Namespace My.Resources
     '''<summary>
     '''  A strongly-typed resource class, for looking up localized strings, etc.
     '''</summary>
-    <Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0"),  _
+    <Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0"),  _
      Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
      Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
      Global.Microsoft.VisualBasic.HideModuleNameAttribute()>  _
@@ -58,6 +58,46 @@ Namespace My.Resources
             Set
                 resourceCulture = value
             End Set
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property ArmorCenter() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("ArmorCenter", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property ArmorCorner() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("ArmorCorner", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property ArmorInvCorner() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("ArmorInvCorner", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property ArmorSide() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("ArmorSide", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
         End Property
         
         '''<summary>
@@ -763,6 +803,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property LargeBlockRadioAntenna() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("LargeBlockRadioAntenna", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property LargeBlockRemoteControl() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("LargeBlockRemoteControl", resourceCulture)
@@ -776,6 +826,16 @@ Namespace My.Resources
         Friend ReadOnly Property LargeBlockSlideDoor() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("LargeBlockSlideDoor", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property LargeBlockSmallAtmosphericThrust() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("LargeBlockSmallAtmosphericThrust", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
@@ -1206,6 +1266,16 @@ Namespace My.Resources
         Friend ReadOnly Property LargeStairs() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("LargeStairs", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property LargeSteelCatwalk() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("LargeSteelCatwalk", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
