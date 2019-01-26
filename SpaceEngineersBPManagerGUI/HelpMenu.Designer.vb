54c92e9b8fb0958ade51806e80e059b79d445762
LordComfy <comfyseven4@gmail.com>
2019-01-26 17:55:35 -0500

    Added help menu form, added icon to help menu, formatted help menu to concurrent theme, updated mod manager.

--- SpaceEngineersBPManagerGUI/HelpMenu.Designer.vb
+++ SpaceEngineersBPManagerGUI/HelpMenu.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class LocalBlueprintManager
+Partial Class HelpMenu
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,19 +22,21 @@ Partial Class LocalBlueprintManager
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(LocalBlueprintManager))
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(HelpMenu))
         Me.SuspendLayout()
         '
-        'LocalBlueprintManager
+        'HelpMenu
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(735, 637)
+        Me.ClientSize = New System.Drawing.Size(953, 613)
         Me.ForeColor = System.Drawing.SystemColors.ButtonHighlight
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.Name = "LocalBlueprintManager"
-        Me.Text = "Blueprint Manager"
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "HelpMenu"
+        Me.Text = "Help Menu"
         Me.ResumeLayout(False)
 
     End Sub
---
+++ SpaceEngineersBPManagerGUI/HelpMenu.vb
@@ -0,0 +1,3 @@
+﻿Public Class HelpMenu
+
+End Class
