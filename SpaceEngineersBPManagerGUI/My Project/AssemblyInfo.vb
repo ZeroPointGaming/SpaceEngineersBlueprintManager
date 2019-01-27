be088b66e58cfaf76abaa08a421574b402b05b93
LordComfy <comfyseven4@gmail.com>
2019-01-26 20:05:39 -0500

    Fixed check for updates + release v1.7.6.0053

--- SpaceEngineersBPManagerGUI/My Project/AssemblyInfo.vb
+++ SpaceEngineersBPManagerGUI/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.7.5.0052")>
-<Assembly: AssemblyFileVersion("1.7.5.0052")>
+<Assembly: AssemblyVersion("1.7.6.0053")>
+<Assembly: AssemblyFileVersion("1.7.6.0053")>
